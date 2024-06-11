b248fc63216a237b7174fa836f53b09ec36866f3
Tishalovich Maxim <tishalovichm@gmail.com>
2024-06-11 14:05:23 +0300

    Add model layer for students api

---
+++ src/main/java/by/tishalovichm/learnspringboot/model/Student.java
@@ -0,0 +1,18 @@
+package by.tishalovichm.learnspringboot.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Student {
+
+    private int id;
+
+    private String firstName;
+
+    private String lastName;
+
+}
